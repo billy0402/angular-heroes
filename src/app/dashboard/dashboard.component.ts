a5ae1f7b69829ea4809494a7762f4aaa4ca3825b
Billy Huang <10646003@ntub.edu.tw>
2020-07-26 16:10:08 +0800

    feat(dashboard): 實作儀表板

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../model/hero';
+import { HeroService } from '../services/hero.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(res => this.heroes = res.slice(1, 5));
   }
 
 }
